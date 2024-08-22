4c6c8f86494bf500ebbd2204f76d066dbbd613cb
Евгения <marakushina970@mail.ru>
2024-08-22 21:28:27 +0300

    Выполнение домашней работы

---
+++ module_2_3.py
@@ -0,0 +1,18 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+start = 0
+print(my_list)
+while start < len(my_list):
+    start = start + 1
+    number = my_list[start]
+    if number == 0:
+        continue
+    elif number < 0:
+        print('Отрицательное число:', number)
+        break
+    elif number == len(my_list):
+        print(number)
+        print('Список закончился')
+    else:
+        print(number)
+
+
