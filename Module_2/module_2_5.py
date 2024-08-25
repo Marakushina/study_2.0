b05c5d41724966d61cd95d80af8e13a6ae4bc1ea
Евгения <marakushina970@mail.ru>
2024-08-26 00:20:33 +0300

    добавление условия для Value

--- Module_2/module_2_5.py
+++ Module_2/module_2_5.py
@@ -1,15 +1,20 @@
 def get_matrix(n, m, value):
     matrix = []
+
     for i in range(n):
         matrix.append(columns:= [])
         for j in range (m):
-            columns.append(value)
+            if value > 1:
+                columns.append(value)
+            else: return matrix
+
+
     return matrix
 
 
-result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 42)
-result3 = get_matrix(4, 2, 13)
+result1 = get_matrix(2, 3, 12)
+result2 = get_matrix(3, 0, 5)
+result3 = get_matrix(5, 5, 0)
 print(result1)
 print(result2)
 print(result3)
