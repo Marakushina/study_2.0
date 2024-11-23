ff8538ce186053a3103c24cc4aa9a59c3922aa51
Евгения <marakushina970@mail.ru>
2024-11-23 12:26:36 +0300

    Д/з

--- Module_2/module_2_5.py
+++ Module_2/module_2_5.py
@@ -2,19 +2,15 @@ def get_matrix(n, m, value):
     matrix = []
 
     for i in range(n):
-        matrix.append(columns:= [])
+        matrix.append(columns := [])
         for j in range (m):
             if value > 1:
                 columns.append(value)
-            else: return matrix
+            else: break
 
 
     return matrix
 
 
-result1 = get_matrix(2, 3, 12)
-result2 = get_matrix(3, 0, 5)
-result3 = get_matrix(5, 5, 0)
+result1 = get_matrix(4, 0, 2)
 print(result1)
-print(result2)
-print(result3)
